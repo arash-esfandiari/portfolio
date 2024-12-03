c513658ae90e124bb13ebcb70fd4a1c81dc3cd23
Arash Esfandiari <arashesfandiari@yahoo.com>
2024-12-03 17:27:14 -0500

    Apps added, mobile view moving up and down fixed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
-import TrackVisibility from 'react-on-screen';
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
@@ -66,27 +65,25 @@ export const Banner = () => {
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-            <TrackVisibility>
-              <div className="animate__animated animate__fadeIn">
-                <span className="tagline">Welcome to my Portfolio</span>
-                <h1>
-                  {`Hi! I'm Arash.`}
-                  <br />
-                  {isSmallScreen ? (
-                    <span>I'm a Software Engineer.</span>
-                  ) : (
-                    <span className="txt-rotate">
-                      <span className="wrap">{text}</span>
-                    </span>
-                  )}
-                </h1>
-                <p>Wandering through the vast universe, driven to make a meaningful impact.</p>
-                <p>Software Engineer at Scale AI, pursuing a Master’s in Artificial Intelligence.</p>
-                <button onClick={() => document.getElementById("connect").scrollIntoView()}>
-                  Let’s Connect <ArrowRightCircle size={25} />
-                </button>
-              </div>
-            </TrackVisibility>
+            <div className="animate__animated animate__fadeIn">
+              <span className="tagline">Welcome to my Portfolio</span>
+              <h1>
+                {`Hi! I'm Arash.`}
+                <br />
+                {isSmallScreen ? (
+                  <span>I'm a Software Engineer.</span>
+                ) : (
+                  <span className="txt-rotate">
+                    <span className="wrap">{text}</span>
+                  </span>
+                )}
+              </h1>
+              <p>Wandering through the vast universe, driven to make a meaningful impact.</p>
+              <p>Software Engineer at Scale AI, pursuing a Master’s in Artificial Intelligence.</p>
+              <button onClick={() => document.getElementById("connect").scrollIntoView()}>
+                Let’s Connect <ArrowRightCircle size={25} />
+              </button>
+            </div>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <div>
